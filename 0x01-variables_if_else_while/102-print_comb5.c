0ba16087da01c7f883e4498df973b4f1b45471a2
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-10-14 22:59:46 +0000

    betty fix

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,33 +8,31 @@
  */
 int main(void)
 {
-	int i, j, k, l, num1, num2;
+int i, j, k, l, num1, num2;
 
-	for (i = 48; i < 58; i++)
-		for (j = 48; j < 58; j++)
-			for (k = 48; k < 58; k++)
-				for (l = 48; l < 58; l++)
+for (i = 48; i < 58; i++)
+	for (j = 48; j < 58; j++)
+		for (k = 48; k < 58; k++)
+			for (l = 48; l < 58; l++)
+			{
+				num1 = (i * 10) + j;
+				num2 = (k * 10) + l;
+
+				if (num1 < num2)
 				{
-					num1 = (i * 10) + j;
-					num2 = (k * 10) + l;
-
-					if (num1 < num2)
-					{
-						putchar(i);
-						putchar(j);
-						putchar(32);
-						putchar(k);
-						putchar(l);
-						if (i == 57 && j == 56 && k == 57 && l == 57)
-						{
-							break;
-						}
-						putchar(44);
-						putchar(32);
-					}
+					putchar(i);
+					putchar(j);
+					putchar(32);
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+						break;
+					putchar(44);
+					putchar(32);
 				}
+			}
 
-	putchar('\n');
+putchar('\n');
 
-	return (0);
+return (0);
 }
