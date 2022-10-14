dd555e8a51f11fc541761ba3f5ec10e0a16daf3b
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-10-14 22:36:32 +0000

    finished the module

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Print combinations of numbers from 00 01 to 98 99
+ *
+ *Return: 0 Success
+ */
+int main(void)
+{
+	int i, j, k, l, num1, num2;
+
+	for (i = 48; i < 58; i++)
+		for (j = 48; j < 58; j++)
+			for (k = 48; k < 58; k++)
+				for (l = 48; l < 58; l++)
+				{
+					num1 = (i * 10) + j;
+					num2 = (k * 10) + l;
+
+					if (num1 < num2)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(32);
+						putchar(k);
+						putchar(l);
+						if (i == 57 && j == 56 && k == 57 && l == 57)
+						{
+							break;
+						}
+						putchar(44);
+						putchar(32);
+					}
+				}
+
+	putchar('\n');
+
+	return (0);
+}
