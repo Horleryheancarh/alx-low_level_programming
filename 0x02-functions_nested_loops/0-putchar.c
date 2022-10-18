74dfee282c9a73bb85e7f7e76e361e5ec9269f58
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-10-18 08:25:15 +0000

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Program to print _putchar
+ *
+ *Return: 0 Success
+ */
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int i, len;
+
+	len = sizeof(str) / sizeof(int);
+	for (i = 0; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+	return (0);
+}
