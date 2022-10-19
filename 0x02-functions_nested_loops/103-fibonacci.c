d496bbc430fe677142c1f5c83bce7329dcc61894
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-10-19 09:09:28 +0000

    sum of even fibonacci numbers under 4m

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: Program find the sum of fibonacci numbers under 4000000
+ *
+ *Return: 0 Success
+ */
+int main(void)
+{
+	long int a, b, temp, sum;
+
+	a = 1;
+	b = 2;
+	while (b < 4000000)
+	{
+		temp = b;
+		b += a;
+		if ((b % 2) == 0)
+			sum += b;
+		a = temp;
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
