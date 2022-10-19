50d1c45dcf40f6403c42c46edfa8acc0c23d3c01
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-10-19 08:24:28 +0000

    add multiples of 3 or 5 under 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: a program to print the sum of all multiples of 3 or 5 under 1024
+ *
+ *Return: 0 Success
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i < 1024; i++)
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+
+	printf("%d\n", sum);
+	return (0);
+}
