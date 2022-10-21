f4c4176ecb4e4ee1c68bfff96197fdbe277f2dcf
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-10-21 04:56:36 +0000

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_diagonal - printa diagonal line as long as the input integer
+ *@n: integer
+ *
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		if (i < n)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
