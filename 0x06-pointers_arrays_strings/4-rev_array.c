0c4d12ed9cb555fb11eab137b9b68b47af83976c
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-10-27 01:04:12 +0000

    reverse array fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  *reverse_array - reverse an array
  *@a: array
@@ -8,15 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i;
+	int i, j, temp;
 
-	i = n - 1;
-	while (i >= 0)
-	{
-		printf("%d", a[i]);
-		if (i != 0)
-			printf(", ");
-		i--;
-	}
-	printf("\n");
+	for (i = 0; i < n - 1; i++)
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 }
