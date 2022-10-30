a22558531db17ba0c431158b342412937b94c786
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-10-30 20:36:18 +0000

    print numbers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,7 +6,7 @@
  *
  *Return: string
  */
-char *rot13 (char *s)
+char *rot13(char *s)
 {
 	int i = 0;
 	int j;
---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *print_number - print integers
+ *@n: integer
+ *
+ *Return: void
+ */
+void print_number(int n)
+{
+	/**
+	 * i - number of digits
+	 * num - the integer
+	 * d - placeholder to cal number of digits
+	 */
+	unsigned int num, d, i;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		num = n * -1;
+	}
+	else
+	{
+		num = n;
+	}
+
+	d = num;
+	i = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		i *= 10;
+	}
+
+	for (; i >= 1; i /= 10)
+	{
+		_putchar(((num / i) % 10) + 48);
+	}
+}
