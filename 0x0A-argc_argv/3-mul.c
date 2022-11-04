da6937e84bd195dfc81d06d5a714a760ba6fdd78
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-11-04 22:52:53 +0000

    multiply two integers from cli

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entrypoint
+ *@argc: number of arguments
+ *@argv: value of arguments
+ *
+ *Return: status
+ */
+int main(int argc, char **argv)
+{
+	if (argc - 1 != 2)
+	{
+		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	exit(EXIT_SUCCESS);
+}
