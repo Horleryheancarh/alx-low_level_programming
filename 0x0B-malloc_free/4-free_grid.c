e1ab6fbe2e486b6ebcc240ce2c30ab5a5c9cc7a1
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-11-07 16:20:45 +0000

    fix: betty styling

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 
 /**
+ *free_grid - free a 2d array malloc
+ *@grid: 2d array
+ *@height: integer height of the 2d array
  *
- *
- *
- *
- *
- *
+ *Return: void
  */
 void free_grid(int **grid, int height)
 {
 	int i;
-	
+
 	for (i = 0; i < height; i++)
 		free(grid[i]);
 
