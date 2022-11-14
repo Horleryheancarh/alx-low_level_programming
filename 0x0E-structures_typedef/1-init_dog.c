a4cab52c63c7b8b1e301ee98c7191ef958f7ef59
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-11-14 21:17:37 +0000

    feat: initialize struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ *init_dog - initialize struct dog
+ *@d: struct dog
+ *@name: string
+ *@age: float
+ *@owner: string
+ *
+ *Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
