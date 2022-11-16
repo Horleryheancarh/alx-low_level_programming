5f366308924838aa3c80908425d2c1d88063294b
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-11-16 22:03:48 +0000

    feat: simple calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+int main(int argc, char **argv)
+{
+	int a, b;
+	int (*op)(int, int);
+
+	if (argc - 1 != 3 || argv[2][1])
+	{
+		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	op = get_op_func(argv[2]);
+
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", op(a, b));
+	exit(EXIT_SUCCESS);
+}
