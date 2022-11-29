3fb4ec86bcdfd3ebcbd70a86d94d2852c4487646
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-11-29 19:33:24 +0000

    feat: get node at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *get_nodeint_at_index - get the node at index
+ *@head: head of a list
+ *@index: index of the node
+ *
+ *Return: node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i < index && head != NULL; i++)
+	{
+		head = head->next;
+	}
+
+	return (head);
+}
