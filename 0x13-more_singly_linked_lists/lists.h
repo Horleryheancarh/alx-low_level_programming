b20b88c62af60b159daf97590bdbad585d355fab
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-11-30 00:09:47 +0000

    feat: advanced part 1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,6 +15,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ *struct listp_s - singly linked list
+ *@p: pointers of node
+ *@next: points to next node
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
 size_t print_listint(const listint_t *);
 size_t listint_len(const listint_t *);
 listint_t *add_nodeint(listint_t **, const int);
@@ -26,5 +37,8 @@ listint_t *get_nodeint_at_index(listint_t *, unsigned int);
 int sum_listint(listint_t *);
 listint_t *insert_nodeint_at_index(listint_t **, unsigned int, int);
 int delete_nodeint_at_index(listint_t **, unsigned int index);
+listint_t *reverse_listint(listint_t **);
+size_t print_listint_safe(const listint_t *);
+
 
 #endif
