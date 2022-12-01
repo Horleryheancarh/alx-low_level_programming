de1769045ee26a58f68ece46e19ca3f0903e7f4f
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-12-01 22:12:59 +0000

    feat: count integers needed to flip a num into another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *flip_bits - return number of bits to be changed to turn n into m
+ *@n: number 1
+ *@m: number 2
+ *
+ *Return: integer
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i;
+
+	for (i = 0; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			i++;
+	}
+
+	return (i);
+}
