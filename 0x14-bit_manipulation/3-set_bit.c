86501940c15de1fc92ebbd2d533b8b29a40689e4
Horleryheancarh <oladipoolayinka7@gmail.com>
2022-12-01 21:57:45 +0000

    feat: set bit at an index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *get_bit - set the value at an index
+ *@n: unsigned long int
+ *@index: index of the bit
+ *
+ *Return: value of the bit
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+        unsigned int i;
+
+        if (index > 63)
+                return (-1);
+
+        i = 1 << index;
+	*n = (*n | i);
+
+        return (1);
+}
+
