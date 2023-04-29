e548dd2904e08bbf65d2a018acaf2649bc58b2c1
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-04-29 14:32:36 +0000

    finish up

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that finds the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """Function that returns perimeter of an island"""
+    peri = 0
+
+    if grid != []:
+        rows = len(grid)
+        cols = len(grid)
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if (i - 1) == -1 or grid[i - 1][j] == 0:
+                    peri += 1
+                if (i + 1) == rows or grid[i + 1][j] == 0:
+                    peri += 1
+                if (j - 1) == -1 or grid[i][j - 1] == 0:
+                    peri += 1
+                if (j + 1) == cols or grid[i][j + 1] == 0:
+                    peri += 1
+
+    return peri
